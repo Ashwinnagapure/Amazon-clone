fd4849944350b8f7eacae20b40d8f6a53a228c44
Ashwin Nagapure <119172063+Ashwinnagapure@users.noreply.github.com>
2024-08-09 22:39:08 +0530

    Add files via upload

---
+++ AmazonClone.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "350px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+  $('.carousel').carousel('cycle');
+
+
+  document.querySelector('footbutton').classList.remove("hover");
